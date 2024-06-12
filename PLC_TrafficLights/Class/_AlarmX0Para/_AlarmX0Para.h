fd9b17a1a874cfd1dc3bb3b14a83d059587f25e0
Klaas <klaas.post@eqraft.com>
2024-06-12 14:00:23 +0200

    Initial Commit

---
+++ PLC_TrafficLights/Class/_AlarmX0Para/_AlarmX0Para.h
@@ -0,0 +1,4 @@
+
+#ifndef ALARM_INITCALL
+  #define ALARM_INITCALL -1 // Defines the init-call in which the initialisation of the alarm objects shall take place. Use -1 for _firstscan call.
+#endif
---
+++ PLC_TrafficLights/Class/_AlarmXBuffer/AlarmXBuffer.h
@@ -0,0 +1,4 @@
+#ifndef MAX_ALARMPARA
+  #define MAX_ALARMPARA  3
+#endif
+
